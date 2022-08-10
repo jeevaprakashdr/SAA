3527ba6015bd85c84576a05fff96dbc08694a3d7
jeeva <jeeva.ramu@codurance.com>
2022-08-10 11:04:47 +0100

    adedd security group and ec2 instance

---
+++ Terraform/vpc/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_key_pair" "ec2_kp" {
+    key_name = "ec2_kp"  
+    public_key = file("ec2_kp.pub")
+}
+
+resource "aws_instance" "public_ec2_A" {
+    ami = "ami-0fb391cce7a602d1f"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.ec2_kp.key_name
+    
+    subnet_id = aws_subnet.public-subnet-A.id
+    vpc_security_group_ids = [ aws_security_group.ssh-allowed.id ]
+
+    tags = {
+      Name = "Public EC2 instance"
+    }
+}
+
+resource "aws_eip" "public_ec2_A_eip" {
+    vpc = true
+    instance = aws_instance.public_ec2_A.id
+}
+
+output "public_ec2_A_ip" {
+    value = aws_eip.public_ec2_A_eip.public_ip
+}
--- Terraform/vpc/network.tf
+++ Terraform/vpc/network.tf
@@ -59,3 +59,25 @@ resource "aws_route_table_association" "main-private-rt-private-subnet-A" {
     subnet_id = aws_subnet.private-subnet-A.id
     route_table_id = aws_route_table.main-private-rt.id    
 }
+
+resource "aws_security_group" "ssh-allowed" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      Name = "ssh-allowed"
+    }
+}
