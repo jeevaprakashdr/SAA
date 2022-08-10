a559ee268087e6f2426c01d0ea2d355dfd3e4855
jeeva <jeeva.ramu@codurance.com>
2022-08-10 11:13:05 +0100

    ec2 private instance

--- Terraform/vpc/ec2.tf
+++ Terraform/vpc/ec2.tf
@@ -16,6 +16,17 @@ resource "aws_instance" "public_ec2_A" {
     }
 }
 
+resource "aws_instance" "private_ec2_A" {
+    ami = "ami-0fb391cce7a602d1f"
+    instance_type = "t2.micro"
+    
+    subnet_id = aws_subnet.private-subnet-A.id
+
+    tags = {
+      "Name" = "Private EC2 instance"
+    }
+}
+
 resource "aws_eip" "public_ec2_A_eip" {
     vpc = true
     instance = aws_instance.public_ec2_A.id
